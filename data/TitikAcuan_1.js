ad366bc8835d1b209d72878380f8441e5ceb90cf
dzakisetiabasith <76066888+dzakisetiabasith@users.noreply.github.com>
2020-12-16 01:39:19 +0700

    Add files via upload

---
+++ data/TitikAcuan_1.js
@@ -0,0 +1,8 @@
+var json_TitikAcuan_1 = {
+"type": "FeatureCollection",
+"name": "TitikAcuan_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 105.258127767681344, -5.42214307387799 ] } }
+]
+}
